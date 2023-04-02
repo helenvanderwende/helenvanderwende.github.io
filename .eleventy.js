c7c6414d8842c14a6b5c9f2483aa436eacbfd93d
Elliot Hursh <elliothursh@gmail.com>
2023-04-01 18:52:18 -0700

    adding images

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,44 @@
+// Import fast-glob package
+const fg = require("fast-glob");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+// Run search for images in /gallery and /sponsors
+const digitalImages = fg.sync(["src/assets/digital/*", "!**/_site"]);
+const drawingImages = fg.sync(["src/assets/drawings/*", "!**/_site"]);
+const paintingImages = fg.sync(["src/assets/paintings/*", "!**/_site"]);
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "/assets" });
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addCollection("digitalImages", function (collection) {
+    return digitalImages.map((image) => {
+      return {
+        url: "/" + image.split("/").slice(1).join("/"),
+        alt: image.split("/").pop(),
+      };
+    });
+  });
+
+  eleventyConfig.addCollection("drawingImages", function (collection) {
+    return drawingImages.map((image) => {
+      return {
+        url: "/" + image.split("/").slice(1).join("/"),
+        alt: image.split("/").pop(),
+      };
+    });
+  });
+
+  eleventyConfig.addCollection("paintingImages", function (collection) {
+    return paintingImages.map((image) => {
+      return {
+        url: "/" + image.split("/").slice(1).join("/"),
+        alt: image.split("/").pop(),
+      };
+    });
+  });
+
   return {
     dir: {
       input: "src",
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/_includes/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./src/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        "2xl": "2.625rem",
+      },
+      fontFamily: {
+        sans: [
+          "PT Sans",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
