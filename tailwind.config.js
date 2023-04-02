95e6e5be3f5b81ca573ad70342235c304973c790
Elliot Hursh <elliothursh@gmail.com>
2023-04-02 16:31:46 -0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/_includes/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
-    "./eleventy.js",
+    "./src/js/gallery.js",
   ],
   theme: {
     extend: {
